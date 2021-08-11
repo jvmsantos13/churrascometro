88a775786b129005331496c6794b83af203c2714
João Victor Monteiro <jvmsantos13@gmail.com>
2021-08-11 17:54:55 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+/*Pegando os valores dos input's e div de resultado, colocando em variáveis */
+
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular () {
+    
+}
